7f67dcd984257919f9ed5ac6f072ac68effb0fd7
rrybarcz <rrybarcz@butler.edu>
2023-02-01 20:24:35 +0000

    SE361-02 Demo

--- WebBrowserGitHubDemo/WebBrowserGitHubDemo/Program.cs
+++ WebBrowserGitHubDemo/WebBrowserGitHubDemo/Program.cs
@@ -11,6 +11,7 @@ namespace WebBrowserGitHubDemo
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// Dr. R was here.
         [STAThread]
         static void Main()
         {
