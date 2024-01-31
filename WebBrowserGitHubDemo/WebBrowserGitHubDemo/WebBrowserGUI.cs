dd4706e39fb64d29782fe8adb03e4ffb6d35464c
rrybarcz <rrybarcz@butler.edu>
2024-01-31 21:20:02 +0000

    Seconds please

--- WebBrowserGitHubDemo/WebBrowserGitHubDemo/WebBrowserGUI.cs
+++ WebBrowserGitHubDemo/WebBrowserGitHubDemo/WebBrowserGUI.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-// Hello World
+// Hello World...Clone Dr. R, are you crazy
 namespace WebBrowserGitHubDemo
 {
     public partial class WebBrowserGUI : Form
