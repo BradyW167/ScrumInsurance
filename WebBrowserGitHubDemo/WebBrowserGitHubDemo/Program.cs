f3c757b171f26a8500c9889b13288dabf2bc3a70
Sibbitt, Jack <jesibbitt@butler.edu>
2025-01-29 09:35:42 -0500

    Update Program.cs

--- WebBrowserGitHubDemo/WebBrowserGitHubDemo/Program.cs
+++ WebBrowserGitHubDemo/WebBrowserGitHubDemo/Program.cs
@@ -14,7 +14,6 @@ namespace WebBrowserGitHubDemo
         /// Dr. R is here.
         /// Dr. R was here.
         /// 
-        /// practice
         /// testing
         [STAThread]
         static void Main()
