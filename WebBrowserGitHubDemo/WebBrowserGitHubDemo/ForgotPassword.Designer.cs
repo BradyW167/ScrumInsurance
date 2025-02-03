4d836f147355a81bf0aa4ef570e741bf992c0e00
Brady Werling <bradywerling@gmail.com>
2025-02-03 11:30:15 -0500

    Renamed ForgotPassword to proper class name

--- WebBrowserGitHubDemo/WebBrowserGitHubDemo/ForgotPassword.Designer.cs
+++ WebBrowserGitHubDemo/WebBrowserGitHubDemo/ForgotPassword.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WebBrowserGitHubDemo
 {
-    partial class Form1
+    partial class ForgotPassword
     {
         /// <summary>
         /// Required designer variable.
@@ -36,26 +36,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 39);
+            this.label1.Location = new System.Drawing.Point(33, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.Size = new System.Drawing.Size(75, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter Email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(41, 62);
+            this.textBox1.Location = new System.Drawing.Point(36, 50);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.Size = new System.Drawing.Size(89, 22);
             this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- WebBrowserGitHubDemo/WebBrowserGitHubDemo/ForgotPassword.cs
+++ WebBrowserGitHubDemo/WebBrowserGitHubDemo/ForgotPassword.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace WebBrowserGitHubDemo
 {
-    public partial class Form1 : Form
+    public partial class ForgotPassword : Form
     {
-        public Form1()
+        public ForgotPassword()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
