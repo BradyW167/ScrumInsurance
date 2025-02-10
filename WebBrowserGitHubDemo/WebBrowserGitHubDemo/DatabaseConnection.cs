b8f88bfd60a1c49f6afc0ba576077721777173f4
Brady Werling <bradywerling@gmail.com>
2025-02-10 09:54:34 -0500

    Added database connection file

---
+++ WebBrowserGitHubDemo/WebBrowserGitHubDemo/DatabaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace LoginForm
+{
+    public class DatabaseConnection
+    {
+        public DatabaseConnection() { }
+
+        private string serverName_;
+        private string databaseName_;
+        private string databasePassword_;
+        private string databaseUsername_;
+        private MySqlConnection connection_;
+    }
+}
