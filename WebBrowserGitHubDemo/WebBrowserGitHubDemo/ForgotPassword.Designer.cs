85e1215ae80841ce2fa7595dc849c82f46095066
Keegan Jackson <kjjackson@butler.edu>
2025-02-12 18:27:18 -0500

    Revert "Deleting unneeded classes"
    This reverts commit c68047c9e7bc2bf379f2eb661d810024b7fd5cce.

---
+++ WebBrowserGitHubDemo/WebBrowserGitHubDemo/ForgotPassword.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WebBrowserGitHubDemo
+{
+    partial class ForgotPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_enterEmailForgotPassword = new System.Windows.Forms.Label();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.txtbx_emailForgotPassword = new System.Windows.Forms.TextBox();
+            this.btn_confirmEmailForgotPassword = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_enterEmailForgotPassword
+            // 
+            this.lbl_enterEmailForgotPassword.AutoSize = true;
+            this.lbl_enterEmailForgotPassword.Location = new System.Drawing.Point(25, 25);
+            this.lbl_enterEmailForgotPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_enterEmailForgotPassword.Name = "lbl_enterEmailForgotPassword";
+            this.lbl_enterEmailForgotPassword.Size = new System.Drawing.Size(60, 13);
+            this.lbl_enterEmailForgotPassword.TabIndex = 0;
+            this.lbl_enterEmailForgotPassword.Text = "Enter Email";
+            // 
+            // txtbx_emailForgotPassword
+            // 
+            this.txtbx_emailForgotPassword.Location = new System.Drawing.Point(27, 40);
+            this.txtbx_emailForgotPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtbx_emailForgotPassword.Name = "txtbx_emailForgotPassword";
+            this.txtbx_emailForgotPassword.Size = new System.Drawing.Size(68, 20);
+            this.txtbx_emailForgotPassword.TabIndex = 1;
+            // 
+            // btn_confirmEmailForgotPassword
+            // 
+            this.btn_confirmEmailForgotPassword.Location = new System.Drawing.Point(100, 40);
+            this.btn_confirmEmailForgotPassword.Name = "btn_confirmEmailForgotPassword";
+            this.btn_confirmEmailForgotPassword.Size = new System.Drawing.Size(75, 23);
+            this.btn_confirmEmailForgotPassword.TabIndex = 2;
+            this.btn_confirmEmailForgotPassword.Text = "Confirm";
+            this.btn_confirmEmailForgotPassword.UseVisualStyleBackColor = true;
+            this.btn_confirmEmailForgotPassword.Click += new System.EventHandler(this.btn_confirmEmailForgotPassword_Click);
+            // 
+            // ForgotPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.btn_confirmEmailForgotPassword);
+            this.Controls.Add(this.txtbx_emailForgotPassword);
+            this.Controls.Add(this.lbl_enterEmailForgotPassword);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "ForgotPassword";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_enterEmailForgotPassword;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.TextBox txtbx_emailForgotPassword;
+        private System.Windows.Forms.Button btn_confirmEmailForgotPassword;
+    }
+}
---
+++ WebBrowserGitHubDemo/WebBrowserGitHubDemo/ForgotPassword.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowserGitHubDemo
+{
+    public partial class ForgotPassword : Form
+    {
+        public ForgotPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_confirmEmailForgotPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
