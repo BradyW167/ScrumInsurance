e110db81b89ae2605d482e1e1cf96d3c7a65e6b8
Brady Werling <bradywerling@gmail.com>
2025-02-24 10:17:51 -0500

    Added login page project

---
+++ loginPage/DatabaseConnection.cs
@@ -0,0 +1,81 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage
+{
+    // Database Connection Class
+    //
+    // By: Dr. Ryan Rybarczyk
+    public class DatabaseConnection
+    {
+        // Default Constructor
+        public DatabaseConnection() { }
+
+        // Class Properties (Attributes)
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+        public string DatabasePassword { get; set; }
+        public string DatabaseUsername { get; set; }
+
+        public MySqlConnection Connection { get; set; }
+        public MySqlDataReader Reader { get; set; }
+        public MySqlCommand Command { get; set; }
+
+        // Attempts to open a connection to a MySQL database.
+        //
+        // Returns TRUE if successful and FALSE if unsuccessful.
+        public bool openConnection()
+        {
+            if (Connection == null)
+            {
+                string connString = string.Format("Server={0}; database={1}; UID={2}; password={3}", ServerName, DatabaseName, DatabaseUsername, DatabasePassword);
+                Connection = new MySqlConnection(connString);
+
+                // Try-Catch to catch exceptions...
+                try
+                {
+                    Connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Closes a MySQL database connection.
+        public void closeConnection()
+        {
+            Connection.Close();
+        }
+
+        public bool selectQuery(string tableName, string[] args)
+        {
+            Command = Connection.CreateCommand();
+
+            Command.CommandText = "SELECT * FROM " + tableName + " WHERE username = @username and password = @password";
+            Command.Parameters.AddWithValue("@username", args[0]);
+            Command.Parameters.AddWithValue("@password", args[1]);
+
+            Reader = Command.ExecuteReader();
+
+            if (Reader.HasRows)
+            {
+                closeConnection();
+                return true;
+            }
+            else
+            {
+                closeConnection();
+                return false;
+            }
+
+        }
+    }
+}
---
+++ loginPage/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage
+{
+    // Login Controller Class
+    //
+    // By: Dr. Ryan Rybarczyk
+    public class LoginController
+    {
+        public LoginController() 
+        {
+            myConnection_ = new DatabaseConnection();
+
+            // We can move this to a property file later to avoid unncessary clutter and hardcoding.
+            myConnection_.ServerName = "trevor.butler.edu";
+            myConnection_.DatabaseName = "";
+            myConnection_.DatabaseUsername = "";
+            myConnection_.DatabasePassword = "";
+
+            username_ = "";
+            password_ = "";
+        }
+
+        public bool validateLogin(string username, string password)
+        {
+            username_ = username;
+            password_ = password;   
+
+            if (password_.Length < 8)
+            {
+                return false;
+            }
+            else
+            {
+                string[] args = new string[2];
+
+                args[0] = username_;
+                args[1] = password_;
+
+                // Change first parameter to actual table name
+                if (myConnection_.selectQuery("login", args))
+                {
+                    return true;
+                }
+            }
+
+            return true;
+        }
+        
+
+        // Trailing underscore to identify local member.
+        private DatabaseConnection myConnection_;
+        private string username_;
+        private string password_;
+    }
+}
