4c5235927af733b9a2ae028ccba46b2e6301676d
Keegan Jackson <kjjackson@butler.edu>
2025-03-02 19:57:44 -0500

    Fixed Spelling

--- ScrumInsurance/Ctrls/ctrlLandingClient.Designer.cs
+++ ScrumInsurance/Ctrls/ctrlLandingClient.Designer.cs
@@ -52,9 +52,10 @@
             this.lblLogout.BackColor = System.Drawing.Color.Transparent;
             this.lblLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLogout.ForeColor = System.Drawing.Color.GhostWhite;
-            this.lblLogout.Location = new System.Drawing.Point(378, 20);
+            this.lblLogout.Location = new System.Drawing.Point(504, 25);
+            this.lblLogout.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLogout.Name = "lblLogout";
-            this.lblLogout.Size = new System.Drawing.Size(45, 13);
+            this.lblLogout.Size = new System.Drawing.Size(59, 17);
             this.lblLogout.TabIndex = 0;
             this.lblLogout.Text = "Log Out";
             this.lblLogout.Click += new System.EventHandler(this.lblLogout_Click);
@@ -64,9 +65,10 @@
             this.lblWelcome.AutoSize = true;
             this.lblWelcome.BackColor = System.Drawing.Color.LightSteelBlue;
             this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWelcome.Location = new System.Drawing.Point(98, 53);
+            this.lblWelcome.Location = new System.Drawing.Point(131, 65);
+            this.lblWelcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblWelcome.Name = "lblWelcome";
-            this.lblWelcome.Size = new System.Drawing.Size(259, 55);
+            this.lblWelcome.Size = new System.Drawing.Size(321, 69);
             this.lblWelcome.TabIndex = 1;
             this.lblWelcome.Text = "Welcome, ";
             // 
@@ -74,13 +76,13 @@
             // 
             this.lblUser.BackColor = System.Drawing.Color.Transparent;
             this.lblUser.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.lblUser.Location = new System.Drawing.Point(367, 6);
+            this.lblUser.Location = new System.Drawing.Point(489, 7);
             this.lblUser.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.lblUser.MaximumSize = new System.Drawing.Size(67, 13);
-            this.lblUser.MinimumSize = new System.Drawing.Size(67, 13);
+            this.lblUser.MaximumSize = new System.Drawing.Size(89, 16);
+            this.lblUser.MinimumSize = new System.Drawing.Size(89, 16);
             this.lblUser.Name = "lblUser";
             this.lblUser.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.lblUser.Size = new System.Drawing.Size(67, 13);
+            this.lblUser.Size = new System.Drawing.Size(89, 16);
             this.lblUser.TabIndex = 2;
             this.lblUser.Text = "testing";
             this.lblUser.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -89,9 +91,9 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.LightSteelBlue;
             this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(337, 36);
+            this.pictureBox1.Size = new System.Drawing.Size(449, 44);
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
@@ -102,9 +104,9 @@
             this.btnLanding.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLanding.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btnLanding.Location = new System.Drawing.Point(0, 0);
-            this.btnLanding.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLanding.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLanding.Name = "btnLanding";
-            this.btnLanding.Size = new System.Drawing.Size(67, 36);
+            this.btnLanding.Size = new System.Drawing.Size(89, 44);
             this.btnLanding.TabIndex = 8;
             this.btnLanding.Text = "Landing";
             this.btnLanding.UseVisualStyleBackColor = false;
@@ -115,10 +117,10 @@
             this.btnAccount.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
             this.btnAccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAccount.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnAccount.Location = new System.Drawing.Point(67, 0);
-            this.btnAccount.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAccount.Location = new System.Drawing.Point(89, 0);
+            this.btnAccount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAccount.Name = "btnAccount";
-            this.btnAccount.Size = new System.Drawing.Size(67, 36);
+            this.btnAccount.Size = new System.Drawing.Size(89, 44);
             this.btnAccount.TabIndex = 9;
             this.btnAccount.Text = "Account";
             this.btnAccount.UseVisualStyleBackColor = false;
@@ -129,10 +131,10 @@
             this.btnSupport.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
             this.btnSupport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSupport.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnSupport.Location = new System.Drawing.Point(134, 0);
-            this.btnSupport.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSupport.Location = new System.Drawing.Point(179, 0);
+            this.btnSupport.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSupport.Name = "btnSupport";
-            this.btnSupport.Size = new System.Drawing.Size(67, 36);
+            this.btnSupport.Size = new System.Drawing.Size(89, 44);
             this.btnSupport.TabIndex = 10;
             this.btnSupport.Text = "Support";
             this.btnSupport.UseVisualStyleBackColor = false;
@@ -143,10 +145,10 @@
             this.btnRequest.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
             this.btnRequest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRequest.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnRequest.Location = new System.Drawing.Point(201, 0);
-            this.btnRequest.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRequest.Location = new System.Drawing.Point(268, 0);
+            this.btnRequest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRequest.Name = "btnRequest";
-            this.btnRequest.Size = new System.Drawing.Size(67, 36);
+            this.btnRequest.Size = new System.Drawing.Size(89, 44);
             this.btnRequest.TabIndex = 11;
             this.btnRequest.Text = "Submit Request";
             this.btnRequest.UseVisualStyleBackColor = false;
@@ -157,10 +159,10 @@
             this.btnSettings.FlatAppearance.BorderColor = System.Drawing.Color.LightSteelBlue;
             this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSettings.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnSettings.Location = new System.Drawing.Point(269, 0);
-            this.btnSettings.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSettings.Location = new System.Drawing.Point(359, 0);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(67, 36);
+            this.btnSettings.Size = new System.Drawing.Size(89, 44);
             this.btnSettings.TabIndex = 12;
             this.btnSettings.Text = "Settings";
             this.btnSettings.UseVisualStyleBackColor = false;
@@ -170,30 +172,29 @@
             this.lblEnterQuery.AutoSize = true;
             this.lblEnterQuery.BackColor = System.Drawing.Color.LightSteelBlue;
             this.lblEnterQuery.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEnterQuery.Location = new System.Drawing.Point(131, 184);
-            this.lblEnterQuery.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEnterQuery.Location = new System.Drawing.Point(175, 226);
             this.lblEnterQuery.Name = "lblEnterQuery";
-            this.lblEnterQuery.Size = new System.Drawing.Size(178, 15);
+            this.lblEnterQuery.Size = new System.Drawing.Size(212, 18);
             this.lblEnterQuery.TabIndex = 13;
             this.lblEnterQuery.Text = "What would you like to do?";
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.pictureBox2.Location = new System.Drawing.Point(46, 43);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Location = new System.Drawing.Point(61, 53);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(356, 381);
+            this.pictureBox2.Size = new System.Drawing.Size(475, 469);
             this.pictureBox2.TabIndex = 22;
             this.pictureBox2.TabStop = false;
             // 
             // btnLogin
             // 
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btnLogin.Location = new System.Drawing.Point(93, 211);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnLogin.Location = new System.Drawing.Point(124, 260);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(264, 29);
+            this.btnLogin.Size = new System.Drawing.Size(352, 36);
             this.btnLogin.TabIndex = 23;
             this.btnLogin.Text = "Make a claim";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -201,10 +202,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button1.Location = new System.Drawing.Point(93, 263);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Location = new System.Drawing.Point(124, 324);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(264, 29);
+            this.button1.Size = new System.Drawing.Size(352, 36);
             this.button1.TabIndex = 24;
             this.button1.Text = "Recieve you funds";
             this.button1.UseVisualStyleBackColor = true;
@@ -212,17 +213,17 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button2.Location = new System.Drawing.Point(93, 311);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Location = new System.Drawing.Point(124, 383);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(264, 29);
+            this.button2.Size = new System.Drawing.Size(352, 36);
             this.button2.TabIndex = 25;
-            this.button2.Text = "Contant your claim manager";
+            this.button2.Text = "Contact your claim manager";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // ctrlLandingClient
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.SteelBlue;
@@ -240,8 +241,9 @@
             this.Controls.Add(this.lblLogout);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ctrlLandingClient";
-            this.Size = new System.Drawing.Size(450, 450);
+            this.Size = new System.Drawing.Size(600, 554);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
