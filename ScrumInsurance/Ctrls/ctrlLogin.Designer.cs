bd87345e936911a83cd4d0848934ecd9001f8cd8
jmangham <jmangham@butler.edu>
2025-03-24 09:58:42 -0400

    Update login screen

--- ScrumInsurance/Ctrls/ctrlLogin.Designer.cs
+++ ScrumInsurance/Ctrls/ctrlLogin.Designer.cs
@@ -39,16 +39,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lblLoginError = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_createAccount
             // 
             this.lbl_createAccount.AutoSize = true;
             this.lbl_createAccount.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.lbl_createAccount.Location = new System.Drawing.Point(335, 421);
+            this.lbl_createAccount.Location = new System.Drawing.Point(584, 473);
+            this.lbl_createAccount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_createAccount.Name = "lbl_createAccount";
-            this.lbl_createAccount.Size = new System.Drawing.Size(98, 16);
+            this.lbl_createAccount.Size = new System.Drawing.Size(81, 13);
             this.lbl_createAccount.TabIndex = 18;
             this.lbl_createAccount.TabStop = true;
             this.lbl_createAccount.Text = "Create Account";
@@ -59,9 +62,10 @@
             // 
             this.lbl_ForgotPass.AutoSize = true;
             this.lbl_ForgotPass.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.lbl_ForgotPass.Location = new System.Drawing.Point(86, 421);
+            this.lbl_ForgotPass.Location = new System.Drawing.Point(397, 473);
+            this.lbl_ForgotPass.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_ForgotPass.Name = "lbl_ForgotPass";
-            this.lbl_ForgotPass.Size = new System.Drawing.Size(116, 16);
+            this.lbl_ForgotPass.Size = new System.Drawing.Size(92, 13);
             this.lbl_ForgotPass.TabIndex = 17;
             this.lbl_ForgotPass.TabStop = true;
             this.lbl_ForgotPass.Text = "Forgot Password?";
@@ -70,10 +74,12 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LightSteelBlue;
             this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(106, 89);
+            this.label3.Location = new System.Drawing.Point(408, 200);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(306, 38);
+            this.label3.Size = new System.Drawing.Size(245, 32);
             this.label3.TabIndex = 16;
             this.label3.Text = "Please log in using your \r\nSCRUM INSURANCE username and password";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -81,10 +87,10 @@
             // btnLogin
             // 
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btnLogin.Location = new System.Drawing.Point(90, 381);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLogin.Location = new System.Drawing.Point(401, 441);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(352, 36);
+            this.btnLogin.Size = new System.Drawing.Size(264, 29);
             this.btnLogin.TabIndex = 15;
             this.btnLogin.Text = "Log In";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -92,10 +98,12 @@
             // 
             // welcomeLabel
             // 
-            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.welcomeLabel.Location = new System.Drawing.Point(58, 39);
+            this.welcomeLabel.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.welcomeLabel.Location = new System.Drawing.Point(394, 28);
+            this.welcomeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.welcomeLabel.Name = "welcomeLabel";
-            this.welcomeLabel.Size = new System.Drawing.Size(403, 50);
+            this.welcomeLabel.Size = new System.Drawing.Size(276, 41);
             this.welcomeLabel.TabIndex = 14;
             this.welcomeLabel.Text = "SCRUM INSURANCE";
             this.welcomeLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -103,20 +111,20 @@
             // txtUsername
             // 
             this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.txtUsername.Location = new System.Drawing.Point(86, 196);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtUsername.Location = new System.Drawing.Point(397, 290);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(352, 36);
+            this.txtUsername.Size = new System.Drawing.Size(265, 30);
             this.txtUsername.TabIndex = 12;
             // 
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.txtPassword.Location = new System.Drawing.Point(86, 285);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtPassword.Location = new System.Drawing.Point(397, 363);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '●';
-            this.txtPassword.Size = new System.Drawing.Size(352, 36);
+            this.txtPassword.Size = new System.Drawing.Size(265, 30);
             this.txtPassword.TabIndex = 13;
             // 
             // label2
@@ -124,9 +132,10 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.LightSteelBlue;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(81, 247);
+            this.label2.Location = new System.Drawing.Point(394, 332);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 25);
+            this.label2.Size = new System.Drawing.Size(82, 20);
             this.label2.TabIndex = 11;
             this.label2.Text = "Password:";
             // 
@@ -135,9 +144,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.LightSteelBlue;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label1.Location = new System.Drawing.Point(81, 167);
+            this.label1.Location = new System.Drawing.Point(394, 267);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.Size = new System.Drawing.Size(87, 20);
             this.label1.TabIndex = 10;
             this.label1.Text = "Username:";
             // 
@@ -146,10 +156,11 @@
             this.lblLoginError.BackColor = System.Drawing.Color.LightSteelBlue;
             this.lblLoginError.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
             this.lblLoginError.ForeColor = System.Drawing.Color.Crimson;
-            this.lblLoginError.Location = new System.Drawing.Point(97, 327);
+            this.lblLoginError.Location = new System.Drawing.Point(406, 397);
+            this.lblLoginError.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblLoginError.Name = "lblLoginError";
             this.lblLoginError.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.lblLoginError.Size = new System.Drawing.Size(333, 42);
+            this.lblLoginError.Size = new System.Drawing.Size(250, 34);
             this.lblLoginError.TabIndex = 19;
             this.lblLoginError.Text = "CLEAR ON INITIALIZE";
             this.lblLoginError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -157,18 +168,31 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.pictureBox1.Location = new System.Drawing.Point(66, 156);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(383, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(395, 290);
+            this.pictureBox1.Size = new System.Drawing.Size(296, 607);
             this.pictureBox1.TabIndex = 20;
             this.pictureBox1.TabStop = false;
             // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.pictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox9.Image = global::ScrumInsurance.Properties.Resources.scrum_logo;
+            this.pictureBox9.Location = new System.Drawing.Point(456, 65);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(156, 121);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox9.TabIndex = 61;
+            this.pictureBox9.TabStop = false;
+            // 
             // ctrlLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
+            this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.lblLoginError);
             this.Controls.Add(this.lbl_createAccount);
             this.Controls.Add(this.lbl_ForgotPass);
@@ -182,9 +206,10 @@
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "ctrlLogin";
-            this.Size = new System.Drawing.Size(541, 528);
+            this.Size = new System.Drawing.Size(1080, 607);
             this.Load += new System.EventHandler(this.ctrlLogin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -203,5 +228,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblLoginError;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox9;
     }
 }
