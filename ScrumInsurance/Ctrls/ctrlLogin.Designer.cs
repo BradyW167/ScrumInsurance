98cf40a420e7556d63583d1636000a73dedf6900
jmangham <jmangham@butler.edu>
2025-03-27 16:50:59 -0400

    GUI changes

--- ScrumInsurance/Ctrls/ctrlLogin.Designer.cs
+++ ScrumInsurance/Ctrls/ctrlLogin.Designer.cs
@@ -228,7 +228,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.BackColor = System.Drawing.Color.Black;
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.lblLoginError);
             this.Controls.Add(this.lbl_createAccount);
