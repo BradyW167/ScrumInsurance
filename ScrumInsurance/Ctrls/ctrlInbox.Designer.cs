32f787dfb92942b35e1226ecc8762432391b6dbb
jesibbitt <jesibbitt@butler.edu>
2025-04-13 17:42:59 -0400

    full functionable inbox

--- ScrumInsurance/Ctrls/ctrlInbox.Designer.cs
+++ ScrumInsurance/Ctrls/ctrlInbox.Designer.cs
@@ -29,12 +29,6 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnNextPage = new System.Windows.Forms.Button();
-            this.btnPrevPage = new System.Windows.Forms.Button();
-            this.lblNumMessages = new System.Windows.Forms.Label();
-            this.btnMessageA = new System.Windows.Forms.Button();
-            this.btnMessageB = new System.Windows.Forms.Button();
-            this.btnMessageC = new System.Windows.Forms.Button();
             this.lblTrademark = new System.Windows.Forms.Label();
             this.lblBottomInformation = new System.Windows.Forms.Label();
             this.lblClaim4 = new System.Windows.Forms.Label();
@@ -46,6 +40,12 @@
             this.pbxBottomPanel = new System.Windows.Forms.PictureBox();
             this.btnLogo = new System.Windows.Forms.PictureBox();
             this.pnlMessages = new System.Windows.Forms.Panel();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblSender = new System.Windows.Forms.Label();
+            this.lblClaim = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblContents = new System.Windows.Forms.Label();
+            this.pnlMessageContents = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxSidePanel)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxBottomPanel)).BeginInit();
@@ -61,83 +61,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(507, 511);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            // 
-            // btnNextPage
-            // 
-            this.btnNextPage.BackColor = System.Drawing.Color.Azure;
-            this.btnNextPage.Location = new System.Drawing.Point(1022, 581);
-            this.btnNextPage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnNextPage.Name = "btnNextPage";
-            this.btnNextPage.Size = new System.Drawing.Size(75, 34);
-            this.btnNextPage.TabIndex = 2;
-            this.btnNextPage.Text = "Next";
-            this.btnNextPage.UseVisualStyleBackColor = false;
-            // 
-            // btnPrevPage
-            // 
-            this.btnPrevPage.BackColor = System.Drawing.Color.Azure;
-            this.btnPrevPage.Location = new System.Drawing.Point(940, 581);
-            this.btnPrevPage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnPrevPage.Name = "btnPrevPage";
-            this.btnPrevPage.Size = new System.Drawing.Size(75, 34);
-            this.btnPrevPage.TabIndex = 3;
-            this.btnPrevPage.Text = "Back";
-            this.btnPrevPage.UseVisualStyleBackColor = false;
-            // 
-            // lblNumMessages
-            // 
-            this.lblNumMessages.AutoSize = true;
-            this.lblNumMessages.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.lblNumMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumMessages.Location = new System.Drawing.Point(953, 619);
-            this.lblNumMessages.Name = "lblNumMessages";
-            this.lblNumMessages.Size = new System.Drawing.Size(136, 20);
-            this.lblNumMessages.TabIndex = 4;
-            this.lblNumMessages.Text = "?-?/? Messages";
-            this.lblNumMessages.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // btnMessageA
-            // 
-            this.btnMessageA.BackColor = System.Drawing.Color.Azure;
-            this.btnMessageA.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnMessageA.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnMessageA.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMessageA.Location = new System.Drawing.Point(1004, 242);
-            this.btnMessageA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnMessageA.Name = "btnMessageA";
-            this.btnMessageA.Size = new System.Drawing.Size(75, 39);
-            this.btnMessageA.TabIndex = 5;
-            this.btnMessageA.Text = "View";
-            this.btnMessageA.UseVisualStyleBackColor = false;
-            this.btnMessageA.Click += new System.EventHandler(this.btnMessageA_Click);
-            // 
-            // btnMessageB
-            // 
-            this.btnMessageB.BackColor = System.Drawing.Color.Azure;
-            this.btnMessageB.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnMessageB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnMessageB.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMessageB.Location = new System.Drawing.Point(1004, 339);
-            this.btnMessageB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnMessageB.Name = "btnMessageB";
-            this.btnMessageB.Size = new System.Drawing.Size(75, 39);
-            this.btnMessageB.TabIndex = 6;
-            this.btnMessageB.Text = "View";
-            this.btnMessageB.UseVisualStyleBackColor = false;
-            // 
-            // btnMessageC
-            // 
-            this.btnMessageC.BackColor = System.Drawing.Color.Azure;
-            this.btnMessageC.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnMessageC.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnMessageC.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMessageC.Location = new System.Drawing.Point(1004, 426);
-            this.btnMessageC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnMessageC.Name = "btnMessageC";
-            this.btnMessageC.Size = new System.Drawing.Size(75, 39);
-            this.btnMessageC.TabIndex = 7;
-            this.btnMessageC.Text = "View";
-            this.btnMessageC.UseVisualStyleBackColor = false;
             // 
             // lblTrademark
             // 
@@ -270,11 +193,83 @@
             this.pnlMessages.Size = new System.Drawing.Size(367, 511);
             this.pnlMessages.TabIndex = 115;
             // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(629, 152);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(204, 31);
+            this.lblHeader.TabIndex = 116;
+            this.lblHeader.Text = "Message Subject";
+            // 
+            // lblSender
+            // 
+            this.lblSender.AutoSize = true;
+            this.lblSender.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblSender.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSender.Location = new System.Drawing.Point(630, 185);
+            this.lblSender.Name = "lblSender";
+            this.lblSender.Size = new System.Drawing.Size(82, 25);
+            this.lblSender.TabIndex = 117;
+            this.lblSender.Text = "Sender: ";
+            // 
+            // lblClaim
+            // 
+            this.lblClaim.AutoSize = true;
+            this.lblClaim.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblClaim.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClaim.Location = new System.Drawing.Point(630, 210);
+            this.lblClaim.Name = "lblClaim";
+            this.lblClaim.Size = new System.Drawing.Size(94, 25);
+            this.lblClaim.TabIndex = 118;
+            this.lblClaim.Text = "Claim ID: ";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(996, 156);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(116, 25);
+            this.lblDate.TabIndex = 119;
+            this.lblDate.Text = "12/12/2024";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblContents
+            // 
+            this.lblContents.AutoSize = true;
+            this.lblContents.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblContents.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContents.Location = new System.Drawing.Point(630, 235);
+            this.lblContents.Name = "lblContents";
+            this.lblContents.Size = new System.Drawing.Size(99, 25);
+            this.lblContents.TabIndex = 120;
+            this.lblContents.Text = "Contents: ";
+            // 
+            // pnlMessageContents
+            // 
+            this.pnlMessageContents.AutoScroll = true;
+            this.pnlMessageContents.BackColor = System.Drawing.Color.Azure;
+            this.pnlMessageContents.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMessageContents.Location = new System.Drawing.Point(645, 265);
+            this.pnlMessageContents.Name = "pnlMessageContents";
+            this.pnlMessageContents.Size = new System.Drawing.Size(447, 358);
+            this.pnlMessageContents.TabIndex = 121;
+            // 
             // ctrlInbox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
+            this.Controls.Add(this.pnlMessageContents);
+            this.Controls.Add(this.lblContents);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblClaim);
+            this.Controls.Add(this.lblSender);
+            this.Controls.Add(this.lblHeader);
             this.Controls.Add(this.pnlMessages);
             this.Controls.Add(this.lblTrademark);
             this.Controls.Add(this.lblBottomInformation);
@@ -286,12 +281,6 @@
             this.Controls.Add(this.pbxSidePanel);
             this.Controls.Add(this.pbxBottomPanel);
             this.Controls.Add(this.btnLogo);
-            this.Controls.Add(this.btnMessageC);
-            this.Controls.Add(this.btnMessageB);
-            this.Controls.Add(this.btnMessageA);
-            this.Controls.Add(this.lblNumMessages);
-            this.Controls.Add(this.btnPrevPage);
-            this.Controls.Add(this.btnNextPage);
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ctrlInbox";
@@ -307,12 +296,6 @@
 
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button btnNextPage;
-        private System.Windows.Forms.Button btnPrevPage;
-        private System.Windows.Forms.Label lblNumMessages;
-        private System.Windows.Forms.Button btnMessageA;
-        private System.Windows.Forms.Button btnMessageB;
-        private System.Windows.Forms.Button btnMessageC;
         private System.Windows.Forms.Label lblTrademark;
         private System.Windows.Forms.Label lblBottomInformation;
         private System.Windows.Forms.Label lblClaim4;
@@ -324,5 +307,11 @@
         private System.Windows.Forms.PictureBox pbxBottomPanel;
         private System.Windows.Forms.PictureBox btnLogo;
         private System.Windows.Forms.Panel pnlMessages;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblSender;
+        private System.Windows.Forms.Label lblClaim;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblContents;
+        private System.Windows.Forms.Panel pnlMessageContents;
     }
 }
--- ScrumInsurance/Ctrls/ctrlInbox.cs
+++ ScrumInsurance/Ctrls/ctrlInbox.cs
@@ -8,6 +8,8 @@ using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using MySqlX.XDevAPI.Relational;
 
 namespace ScrumInsurance
 {
@@ -17,9 +19,15 @@ namespace ScrumInsurance
         public ctrlInbox(Session session, DatabaseController DBController)
         {
             InitializeComponent();
+            lblHeader.Hide();
+            lblSender.Hide();
+            lblDate.Hide();
+            lblContents.Hide();
+            lblClaim.Hide();
+            pnlMessageContents.Hide();
 
             //these are the columns we want to grab for the select query
-            string[] columns = { "Sender", "Message_Date", "Message_Subject"};
+            string[] columns = { "Sender", "Message_Date", "Message_Subject", "Message_ID"};
 
             //these set the args. 
             Dictionary<String, Object> args = new Dictionary<String, Object>();
@@ -30,7 +38,7 @@ namespace ScrumInsurance
             for (int i = 0; i < messageList.Count; i++)
             {
                 object[] messageDetails = messageList[i];
-                addMessage(messageDetails[0].ToString(), messageDetails[2].ToString(), messageDetails[1].ToString());
+                addMessage(messageDetails[0].ToString(), messageDetails[2].ToString(), messageDetails[1].ToString(), messageDetails[3]);
                 Console.WriteLine(messageDetails[0] + ", "+ messageDetails[1] + ", " + messageDetails[2] + ", ");
             }
 
@@ -38,7 +46,7 @@ namespace ScrumInsurance
 
         }
 
-        private void addMessage(string sender, string subject, string date)
+        private void addMessage(string sender, string subject, string date, object id)
         {
             messageCount_++;
             //view button
@@ -50,6 +58,8 @@ namespace ScrumInsurance
             btn.TextAlign = ContentAlignment.MiddleCenter;
             btn.Width = 40;
             btn.Location = new Point(170, (messageCount_ * 60) -25);
+            //we store the messageID in the buttons tag so that it can be called later using the sender in the click function. 
+            btn.Tag = id;
             btn.Click += new System.EventHandler(this.btnMessageA_Click);
             pnlMessages.Controls.Add(btn);
             
@@ -68,7 +78,61 @@ namespace ScrumInsurance
 
         private void btnMessageA_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("blah");
+            //this unloads any previous control in the panel. 
+            if (pnlMessageContents.Controls.Count > 0)
+            {
+                pnlMessageContents.Controls.RemoveAt(0);
+            }
+
+            //this creates a new button using sender as a button
+            Button btn = sender as Button;
+            //because the sender is always a button created in the addMessage method, it will have the id stored in the tag which we can grab. 
+            int messageId = int.Parse((btn.Tag).ToString());
+
+            //columns for query 
+            string[] messageColumns = { "Sender", "Message_Date", "Message_Subject",  "Message_Content", "Claim_ID"};
+
+            //these set the args. 
+            Dictionary<String, Object> args = new Dictionary<String, Object>();
+            args.Add("Message_ID", messageId);
+
+            //call the query and get the details
+            Dictionary<int, object[]> messageList = DBController.MessageInformation(args, messageColumns);
+            object[] messageDetails = messageList[0]; // because messageID is unique, we only need [0]
+
+            //assign to lables and show
+            lblSender.Text = "Sender: " + messageDetails[0].ToString();
+            lblSender.Show();
+
+            lblDate.Text = messageDetails[1].ToString().Split(' ')[0];
+            lblDate.Show();
+
+            lblHeader.Text = messageDetails[2].ToString();
+            lblHeader.Show();
+
+            String messageContent = messageDetails[3].ToString();
+            pnlMessageContents.Show();
+
+            lblClaim.Text = "Claim ID: " + messageDetails[4].ToString();
+            lblClaim.Show();
+
+
+            //adds the contents to the panel. 
+
+            Label msg = new Label();
+            msg.BackColor = Color.Azure;
+            msg.TextAlign = ContentAlignment.TopLeft;
+            msg.Text = messageContent;
+            msg.Location = new Point(22,10);
+            msg.Width = 250;
+            msg.Height = 900;
+            pnlMessageContents.Controls.Add(msg);
+
+        }
+
+        private void btnMessageC_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
