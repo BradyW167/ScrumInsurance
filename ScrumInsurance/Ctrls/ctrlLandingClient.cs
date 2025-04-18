86226ad0fdb886a402f05d4204fa25b0bf94404b
Brady Werling <bradywerling@gmail.com>
2025-04-18 00:45:51 -0400

    Landing Client page loads claim in during the Load event and not in the constructor

--- ScrumInsurance/Ctrls/ctrlLandingClient.cs
+++ ScrumInsurance/Ctrls/ctrlLandingClient.cs
@@ -12,15 +12,14 @@ namespace ScrumInsurance
 {
     public partial class ctrlLandingClient : ScrumUserControl
     {
-        public ctrlLandingClient(Session session)
+        public ctrlLandingClient()
         {
             InitializeComponent();
-            Session = session;
-            RefreshClaims();
         }
 
         private void ctrlLanding_Load(object sender, EventArgs e)
         {
+            RefreshClaims();
         }
 
         private void lblLogout_Click(object sender, EventArgs e)
@@ -45,29 +44,44 @@ namespace ScrumInsurance
 
         private void RefreshClaims()
         {
+            // Convert the Session Account object to a Client object
+            Client client = Session.UserAccount as Client;
+
+            // Stores names of each label for displaying claims
             Label[] lblClaims = new Label[] { lblClaim1, lblClaim2, lblClaim3, lblClaim4 };
-            if (((Client)Session.UserAccount).Claims.Count > 0)
+
+            // If the client has any claims...
+            if (client.Claims.Count > 0)
             {
-                lblNumClaims.Text = ((Client)Session.UserAccount).Claims.Count + " claims ";
-                for (int i = 0; i < lblClaims.Length; i++)
+                // Display claim count in lblNumClaims
+                lblNumClaims.Text = "You Have " + client.Claims.Count + " Claims";
+
+                // Loop through each claim, up to 4
+                for (int i = 0; i < 4; i++)
                 {
-                    if (((Client)Session.UserAccount).Claims.Count > i)
+                    // If claim i does not exist...
+                    if (client.Claims[i] != null)
                     {
-                        lblClaims[i].Text = ((Client)Session.UserAccount).Claims[i].Title + "\nStatus: " + ((Client)Session.UserAccount).Claims[i].Status;
-                    }
-                    else
-                    {
+                        // Hide label for claim i and continue to next claim
                         lblClaims[i].Visible = false;
-                    }
+                        continue;
+                    };
+
+                    // Set claim label i's text to claim i's info
+                    lblClaims[i].Text = client.Claims[i].Title + "\nStatus: " + client.Claims[i].Status;
+                    
+                    // Make claim i visible
+                    lblClaims[i].Visible = true;
                 }
             }
+            // Else client has no claims
             else
             {
-                lblNumClaims.Text = "No claims";
-                foreach (Label label in lblClaims)
-                {
-                    label.Visible = false;
-                }
+                // Show user they have no claims
+                lblNumClaims.Text = "No Claims";
+
+                // Hide all claim labels
+                foreach (Label label in lblClaims) { label.Visible = false; }
             }
         }
 
