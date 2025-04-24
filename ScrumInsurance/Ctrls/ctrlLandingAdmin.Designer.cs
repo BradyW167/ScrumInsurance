3b6ed2c364a942a7774bec8e9dcafacac2464ff2
Brady Werling <bradywerling@gmail.com>
2025-04-24 14:41:15 -0400

    Data grid for accounts altered

--- ScrumInsurance/Ctrls/ctrlLandingAdmin.Designer.cs
+++ ScrumInsurance/Ctrls/ctrlLandingAdmin.Designer.cs
@@ -28,51 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblHeader = new System.Windows.Forms.Label();
             this.txtInputUser = new System.Windows.Forms.TextBox();
             this.lblUser = new System.Windows.Forms.Label();
             this.dgvUserinfo = new System.Windows.Forms.DataGridView();
-            this.Username = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Password = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Question = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Answer = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cmbSelectRole = new System.Windows.Forms.ComboBox();
             this.lblSelectRole = new System.Windows.Forms.Label();
             this.pbxPillar2 = new System.Windows.Forms.PictureBox();
+            this.btnCommit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUserinfo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxPillar2)).BeginInit();
             this.SuspendLayout();
             // 
-            // lblHeader
-            // 
-            this.lblHeader.AutoSize = true;
-            this.lblHeader.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.lblHeader.Font = new System.Drawing.Font("Microsoft Tai Le", 20F, System.Drawing.FontStyle.Bold);
-            this.lblHeader.Location = new System.Drawing.Point(400, 59);
-            this.lblHeader.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(234, 34);
-            this.lblHeader.TabIndex = 0;
-            this.lblHeader.Text = "Welcome, Admin";
-            // 
             // txtInputUser
             // 
             this.txtInputUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtInputUser.Location = new System.Drawing.Point(283, 118);
+            this.txtInputUser.Location = new System.Drawing.Point(283, 81);
             this.txtInputUser.Margin = new System.Windows.Forms.Padding(2);
             this.txtInputUser.Name = "txtInputUser";
             this.txtInputUser.Size = new System.Drawing.Size(200, 30);
             this.txtInputUser.TabIndex = 1;
-            this.txtInputUser.TextChanged += new System.EventHandler(this.txtInputUser_TextChanged);
             this.txtInputUser.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInputUser_KeyDown);
+            this.txtInputUser.Leave += new System.EventHandler(this.txtInputUser_Leave);
             // 
             // lblUser
             // 
             this.lblUser.AutoSize = true;
             this.lblUser.BackColor = System.Drawing.Color.LightSteelBlue;
             this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblUser.Location = new System.Drawing.Point(281, 97);
+            this.lblUser.Location = new System.Drawing.Point(281, 60);
             this.lblUser.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUser.Name = "lblUser";
             this.lblUser.Size = new System.Drawing.Size(111, 17);
@@ -81,15 +64,10 @@
             // 
             // dgvUserinfo
             // 
+            this.dgvUserinfo.AllowUserToAddRows = false;
             this.dgvUserinfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvUserinfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Username,
-            this.Password,
-            this.Email,
-            this.Question,
-            this.Answer});
-            this.dgvUserinfo.Location = new System.Drawing.Point(133, 171);
-            this.dgvUserinfo.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvUserinfo.Location = new System.Drawing.Point(133, 135);
+            this.dgvUserinfo.Margin = new System.Windows.Forms.Padding(0);
             this.dgvUserinfo.Name = "dgvUserinfo";
             this.dgvUserinfo.RowHeadersWidth = 62;
             this.dgvUserinfo.RowTemplate.Height = 28;
@@ -97,51 +75,16 @@
             this.dgvUserinfo.TabIndex = 3;
             this.dgvUserinfo.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUserinfo_CellValueChanged);
             // 
-            // Username
-            // 
-            this.Username.HeaderText = "Username";
-            this.Username.MinimumWidth = 8;
-            this.Username.Name = "Username";
-            this.Username.Width = 150;
-            // 
-            // Password
-            // 
-            this.Password.HeaderText = "Password";
-            this.Password.MinimumWidth = 8;
-            this.Password.Name = "Password";
-            this.Password.Width = 150;
-            // 
-            // Email
-            // 
-            this.Email.HeaderText = "Email";
-            this.Email.MinimumWidth = 8;
-            this.Email.Name = "Email";
-            this.Email.Width = 150;
-            // 
-            // Question
-            // 
-            this.Question.HeaderText = "Question";
-            this.Question.MinimumWidth = 8;
-            this.Question.Name = "Question";
-            this.Question.Width = 150;
-            // 
-            // Answer
-            // 
-            this.Answer.HeaderText = "Answer";
-            this.Answer.MinimumWidth = 8;
-            this.Answer.Name = "Answer";
-            this.Answer.Width = 150;
-            // 
             // cmbSelectRole
             // 
-            this.cmbSelectRole.DropDownHeight = 80;
+            this.cmbSelectRole.DropDownHeight = 100;
             this.cmbSelectRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbSelectRole.DropDownWidth = 200;
             this.cmbSelectRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbSelectRole.FormattingEnabled = true;
             this.cmbSelectRole.IntegralHeight = false;
             this.cmbSelectRole.ItemHeight = 25;
-            this.cmbSelectRole.Location = new System.Drawing.Point(586, 116);
+            this.cmbSelectRole.Location = new System.Drawing.Point(586, 79);
             this.cmbSelectRole.Margin = new System.Windows.Forms.Padding(0);
             this.cmbSelectRole.Name = "cmbSelectRole";
             this.cmbSelectRole.Size = new System.Drawing.Size(200, 33);
@@ -153,7 +96,7 @@
             this.lblSelectRole.AutoSize = true;
             this.lblSelectRole.BackColor = System.Drawing.Color.LightSteelBlue;
             this.lblSelectRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblSelectRole.Location = new System.Drawing.Point(583, 97);
+            this.lblSelectRole.Location = new System.Drawing.Point(583, 60);
             this.lblSelectRole.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSelectRole.Name = "lblSelectRole";
             this.lblSelectRole.Size = new System.Drawing.Size(80, 17);
@@ -163,6 +106,7 @@
             // pbxPillar2
             // 
             this.pbxPillar2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.pbxPillar2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pbxPillar2.Location = new System.Drawing.Point(30, 30);
             this.pbxPillar2.Margin = new System.Windows.Forms.Padding(2);
             this.pbxPillar2.Name = "pbxPillar2";
@@ -170,17 +114,29 @@
             this.pbxPillar2.TabIndex = 82;
             this.pbxPillar2.TabStop = false;
             // 
+            // btnCommit
+            // 
+            this.btnCommit.AutoSize = true;
+            this.btnCommit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnCommit.Location = new System.Drawing.Point(453, 527);
+            this.btnCommit.Name = "btnCommit";
+            this.btnCommit.Size = new System.Drawing.Size(174, 35);
+            this.btnCommit.TabIndex = 85;
+            this.btnCommit.Text = "Commit Changes";
+            this.btnCommit.UseVisualStyleBackColor = true;
+            this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
+            // 
             // ctrlLandingAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
+            this.Controls.Add(this.btnCommit);
             this.Controls.Add(this.lblSelectRole);
             this.Controls.Add(this.cmbSelectRole);
             this.Controls.Add(this.dgvUserinfo);
             this.Controls.Add(this.lblUser);
             this.Controls.Add(this.txtInputUser);
-            this.Controls.Add(this.lblHeader);
             this.Controls.Add(this.pbxPillar2);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "ctrlLandingAdmin";
@@ -193,18 +149,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblHeader;
         private System.Windows.Forms.TextBox txtInputUser;
         private System.Windows.Forms.Label lblUser;
         private System.Windows.Forms.DataGridView dgvUserinfo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Username;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Password;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Question;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Answer;
         private System.Windows.Forms.PictureBox pbxPillar2;
         private System.Windows.Forms.ComboBox cmbSelectRole;
         private System.Windows.Forms.Label lblSelectRole;
+        private System.Windows.Forms.Button btnCommit;
     }
 }
--- ScrumInsurance/Ctrls/ctrlLandingAdmin.cs
+++ ScrumInsurance/Ctrls/ctrlLandingAdmin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
@@ -9,29 +10,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Org.BouncyCastle.Utilities;
+using Renci.SshNet.Security;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ScrumInsurance.Ctrls
 {
     public partial class ctrlLandingAdmin : ScrumUserControl
     {
-        //private List<Account> Accounts {  get; set; }
-        public DataSet Accounts { get; set; }
-        public BindingSource bindingSource {get; set;}
-        private Dictionary<string, string> args;
+        public DataSet AccountDataSet { get; set; }
+        public BindingSource BindingSource {get; set;}
+        private Dictionary<string, string> WhereConditions;
 
         public ctrlLandingAdmin(ScrumUserControl oldCtrl) : base(oldCtrl)
         {
 
             InitializeComponent();
 
-            dgvUserinfo.Columns.Clear();
+            // Binds datagridview to dataset, so changing one changes the other
+            BindingSource = new BindingSource();
 
-            //Binds datagridview to dataset, so changing one changes the other
-            bindingSource = new BindingSource();
-            dgvUserinfo.DataSource = bindingSource;
-            Accounts = DBController.GetAccounts(new Dictionary<string, string>());
-            bindingSource.DataSource = Accounts.Tables[0];
+            // Stores the Data Set
+            AccountDataSet = DBController.GetAccountDataSet();
+
+            BindingSource.DataSource = AccountDataSet.Tables["Accounts"];
+
+            dgvUserinfo.DataSource = BindingSource;
+
+            // Sets the data grid to auto size all columns
+            dgvUserinfo.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
 
             Dictionary<string, string> role_pairs = new Dictionary<string, string>()
             {
@@ -45,50 +51,85 @@ namespace ScrumInsurance.Ctrls
             cmbSelectRole.DataSource = new BindingSource(role_pairs, null);
             cmbSelectRole.DisplayMember = "Key";
             cmbSelectRole.ValueMember = "Value";
-        }
 
-        private void txtUserInput_KeyDown(object sender, KeyEventArgs e)
-        {
+            cmbSelectRole.DropDownHeight = cmbSelectRole.ItemHeight * (cmbSelectRole.Items.Count + 1);
 
-        }
-
-        private void txtInputUser_KeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void CheckDatabase()
-        {
-            // Store the selected role from the role pair dictionary stored in this combo box
-            args = new Dictionary<string, string> { { "role", ((KeyValuePair<string, string>)cmbSelectRole.SelectedItem).Value } };
-
-            //If username is filled in, add it as argument
-            if (!txtInputUser.Text.Equals(""))
-            {
-                args.Add("username", txtInputUser.Text);
-            }
-
-            Accounts = DBController.GetAccounts(args);
-
-            bindingSource.DataSource = null;
-            bindingSource.DataSource = Accounts.Tables[0];
-
-            dgvUserinfo.Refresh();
+            WhereConditions = new Dictionary<string, string>();
         }
 
         private void cmbSelectRole_SelectionChangeCommitted(object sender, EventArgs e)
         {
-            CheckDatabase();
+            ReloadDataGrid();
         }
 
-        private void txtInputUser_TextChanged(object sender, EventArgs e)
+        private void txtInputUser_KeyDown(object sender, KeyEventArgs e)
         {
-            CheckDatabase();
+            if (e.KeyCode == Keys.Enter)
+            {
+                ReloadDataGrid();
+            }
+        }
+
+        private void txtInputUser_Leave(object sender, EventArgs e)
+        {
+            ReloadDataGrid();
+        }
+
+        private void ReloadDataGrid()
+        {
+            // Store the selected role in the combobox dropdown
+            string selected_role = ((KeyValuePair<string, string>)cmbSelectRole.SelectedItem).Value;
+
+            // If not selecting all roles
+            if (selected_role != "*")
+            {
+                // Store the selected role from the role pair dictionary stored in this combo box
+                WhereConditions.Add("role", selected_role);
+            }
+
+            //If username is filled in, add it as argument
+            if (!txtInputUser.Text.Equals(""))
+            {
+                WhereConditions.Add("username", txtInputUser.Text);
+            }
+
+            // Stores the dataset of accounts
+            AccountDataSet = DBController.GetAccountDataSet(WhereConditions);
+
+            // Clear the data source for the table to force an update
+            BindingSource.DataSource = null;
+
+            // Input the new binding data source as the Account Data Set 'Accounts' table
+            BindingSource.DataSource = AccountDataSet.Tables["Accounts"];
+
+            // dgvUserinfo.Refresh();
+
+            // Clear the where conditions after use
+            WhereConditions.Clear();
         }
 
         private void dgvUserinfo_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
-            DBController.UpdateAccounts(args, Accounts);
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                // Access updated data
+                var updatedRow = AccountDataSet.Tables["Accounts"].Rows[e.RowIndex];
+                var updatedValue = updatedRow[e.ColumnIndex];
+            }
+        }
+
+        private void btnCommit_Click(object sender, EventArgs e)
+        {
+            // Force commit of any current edit in progress
+            dgvUserinfo.EndEdit();
+
+            // Optional: also commit to DataTable if bound via BindingSource
+            if (dgvUserinfo.DataSource is BindingSource bs)
+            {
+                bs.EndEdit();
+            }
+
+            DBController.CommitAccountChanges();
         }
     }
 }
